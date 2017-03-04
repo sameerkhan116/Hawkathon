34214fc721b42803f8300492fa87b99af07e4e6c
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-04 02:32:29 -0500

    Main website

---
+++ js/data-request.js
@@ -0,0 +1,7 @@
+var name = document.getElementById('inputName').value;
+var email = document.getElementById('inputEmail').value;
+var age = document.getElementById('age').value;
+var sexRadio = document.getElementByName('optionsRadios').value;
+var bloodGroup = document.getElementById('select').value;
+var submit = document.getElementById('submit').value;
+var cancel = document.getElementById('cancel').value;
